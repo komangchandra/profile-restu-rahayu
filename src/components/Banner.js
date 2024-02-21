b262a72ec006d1441ec7cc6e1ae96fae9cd165e3
komangchandra <komangchandraaa1@gmail.com>
2024-02-22 03:09:33 +0700

    feat: membuat banner/hero

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,94 @@
-import React from 'react';
+import React from "react";
+import Image from "../assets/avatar.svg";
+import { FaInstagram, FaTiktok } from "react-icons/fa";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <section
+      id="profil"
+      className="min-h-[85vh] lg:min-h-[96vh] lg:pt-28 pt-20">
+      <div className="container mx-auto mt-0">
+        <div className="lg:flex lg:flex-row justify-between items-center">
+          {/* text */}
+          <div>
+            {/* My Name */}
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[45px] font-bold lg:text-[80px] lg:mb-4 mb-1 leading-[1]">
+              STT Restu Rahayu
+            </motion.h1>
+            {/* My Role */}
+            <motion.div
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-2 text-[33px] lg:text-[60px] font-secondary font-semibold leading-[1]">
+              <TypeAnimation
+                sequence={[
+                  "Harapan Banjar",
+                  2000,
+                  "Baleganjur Werdi Rahayu",
+                  2000,
+                ]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+            </motion.div>
+            {/* My Description */}
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-2">
+              Selamat datang di{" "}
+              <span className="font-semibold">Restu Rahayu</span>. Nyalakan api{" "}
+              <span className="font-semibold">Kreativitas🔥</span>
+              dan <span className="font-semibold">Pemimpin Muda</span> yang
+              menjunjung tinggi Pancasila
+            </motion.p>
+            {/* My Sosial */}
+            <motion.div
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              className="flex text-[20px] mb-5 lg:mb-0 items-center">
+              <a
+                href="https://www.instagram.com/resturahayyyu/"
+                target="_blank"
+                className="mr-5">
+                <FaInstagram />
+              </a>
+              <a
+                href="https://www.tiktok.com/@restu.rahayupp"
+                target="_blank"
+                className="mr-5">
+                <FaTiktok />
+              </a>
+            </motion.div>
+          </div>
+          {/* foto */}
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.7 }}
+            className="lg:max-w-[482px]">
+            <img src={Image} />
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,82 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-scroll";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="nav fixed w-full z-10">
+      <nav className="container mx-auto mt-0">
+        <div className="flex flex-col lg:flex-row">
+          <div className="flex items-center justify-between px-4 py-4 lg:py-0">
+            <div className="lg:w-52">
+              <a href="" className="font-semibold uppercase text-white">
+                STT Restu Rahayu
+              </a>
+            </div>
+            <div>
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="block text-white focus:outline-none lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="h-6 w-6">
+                  <path
+                    className={!isOpen ? "block" : "hidden"}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                  <path
+                    className={isOpen ? "block" : "hidden"}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18 18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+          {/* Nav List */}
+          <div
+            className={`${
+              isOpen ? "block" : "hidden"
+            } lg:flex w-full flex-col py-4 lg:flex-row lg:py-0 font-semibold `}>
+            <div className="ms-auto flex flex-col lg:flex-row">
+              <Link
+                to="profile"
+                smooth={true}
+                className="cursor-pointer block px-4 py-2 lg:py-5 text-gradient">
+                Profil
+              </Link>
+              <Link
+                to="profile"
+                smooth={true}
+                className="cursor-pointer block px-4 py-2 lg:py-5 text-gradient">
+                Tentang Kami
+              </Link>
+              <Link
+                to="profile"
+                smooth={true}
+                className="cursor-pointer block px-4 py-2 lg:py-5 text-gradient">
+                Galery
+              </Link>
+              <Link
+                to="profile"
+                smooth={true}
+                className="cursor-pointer block px-4 py-2 lg:py-5 text-gradient">
+                Kontak
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Nav;
